fa797ce6b244f704f5ecb6d893401b8ec5725ca5
IDOMATH <btdothemath@gmail.com>
2023-12-19 20:50:28 -0600

    Setting up some basics

---
+++ backpack/backpack.go
@@ -0,0 +1,50 @@
+package backpack
+
+import "net/http"
+
+type Backpack struct {
+	Port   int
+	Routes []Route
+}
+
+type Route struct {
+	Method  string
+	Url     string
+	Handler http.HandlerFunc
+}
+
+func (b *Backpack) Get(url string, handler http.HandlerFunc) {
+	route := Route{
+		Method:  "GET",
+		Url:     url,
+		Handler: handler,
+	}
+	b.Routes = append(b.Routes, route)
+}
+
+func (b *Backpack) Post(url string, handler http.HandlerFunc) {
+	route := Route{
+		Method:  "POST",
+		Url:     url,
+		Handler: handler,
+	}
+	b.Routes = append(b.Routes, route)
+}
+
+func (b *Backpack) Put(url string, handler http.HandlerFunc) {
+	route := Route{
+		Method:  "PUT",
+		Url:     url,
+		Handler: handler,
+	}
+	b.Routes = append(b.Routes, route)
+}
+
+func (b *Backpack) Delete(url string, handler http.HandlerFunc) {
+	route := Route{
+		Method:  "DELETE",
+		Url:     url,
+		Handler: handler,
+	}
+	b.Routes = append(b.Routes, route)
+}
