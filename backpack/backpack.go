91796c90a2b6213e5949e14e46f80e6abdfe37ee
IDOMATH <btdothemath@gmail.com>
2023-12-24 22:23:14 -0600

    Testing out middleware methods

--- backpack/backpack.go
+++ backpack/backpack.go
@@ -7,7 +7,7 @@ import (
 
 type Backpack struct {
 	Port      int
-	Routes    []Route
+	Routes    map[string]Route
 	Handle405 http.HandlerFunc
 }
 
@@ -21,71 +21,103 @@ type Route struct {
 func (b *Backpack) Serve() {
 	fmt.Printf("Running on port: %d", b.Port)
 	for _, route := range b.Routes {
+		for _, middleware := range route.Middleware {
+			middleware()
+		}
 		http.HandleFunc(route.Url, route.Handler)
 	}
 	http.ListenAndServe(fmt.Sprintf(":%d", b.Port), nil)
 }
 
 func (b *Backpack) Get(url string, handler http.HandlerFunc) {
+	acceptedMethod := "GET"
 	hf := func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "GET" {
+		if r.Method == acceptedMethod {
 			handler(w, r)
 		} else {
-			b.Handle405(w, r)
+			if b.Handle405 != nil {
+				b.Handle405(w, r)
+			} else {
+				DefaultHandle405(w, r)
+			}
 		}
 	}
 	route := Route{
-		Method:  "GET",
+		Method:  acceptedMethod,
 		Url:     url,
 		Handler: hf,
 	}
-	b.Routes = append(b.Routes, route)
+	b.Routes[acceptedMethod+url] = route
 }
 
 func (b *Backpack) Post(url string, handler http.HandlerFunc) {
+	acceptedMethod := "POST"
 	hf := func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "POST" {
+		if r.Method == acceptedMethod {
 			handler(w, r)
 		} else {
-			b.Handle405(w, r)
+			if b.Handle405 != nil {
+				b.Handle405(w, r)
+			} else {
+				DefaultHandle405(w, r)
+			}
 		}
 	}
 	route := Route{
-		Method:  "POST",
+		Method:  acceptedMethod,
 		Url:     url,
 		Handler: hf,
 	}
-	b.Routes = append(b.Routes, route)
+	b.Routes[acceptedMethod+url] = route
 }
 
 func (b *Backpack) Put(url string, handler http.HandlerFunc) {
+	acceptedMethod := "PUT"
 	hf := func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "PUT" {
+		if r.Method == acceptedMethod {
 			handler(w, r)
 		} else {
-			b.Handle405(w, r)
+			if b.Handle405 != nil {
+				b.Handle405(w, r)
+			} else {
+				DefaultHandle405(w, r)
+			}
 		}
 	}
 	route := Route{
-		Method:  "PUT",
+		Method:  acceptedMethod,
 		Url:     url,
 		Handler: hf,
 	}
-	b.Routes = append(b.Routes, route)
+	b.Routes[acceptedMethod+url] = route
 }
 
 func (b *Backpack) Delete(url string, handler http.HandlerFunc) {
+	acceptedMethod := "DELETE"
 	hf := func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "GET" {
+		if r.Method == acceptedMethod {
 			handler(w, r)
 		} else {
-			b.Handle405(w, r)
+			if b.Handle405 != nil {
+				b.Handle405(w, r)
+			} else {
+				DefaultHandle405(w, r)
+			}
 		}
 	}
 	route := Route{
-		Method:  "DELETE",
+		Method:  acceptedMethod,
 		Url:     url,
 		Handler: hf,
 	}
-	b.Routes = append(b.Routes, route)
+	b.Routes[acceptedMethod+url] = route
+}
+
+func (b *Backpack) RegisterMiddleware(route string, middleware func()) {
+	registerTo := b.Routes[route]
+	registerTo.Middleware = append(registerTo.Middleware, middleware)
+}
+
+func DefaultHandle405(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Method not supported"))
 }
--- main.go
+++ main.go
@@ -1,18 +1,20 @@
 package main
 
 import (
+	"fmt"
 	backpack "github.com/idomath/CheetahWeb/backpack"
 	"net/http"
 )
 
 func main() {
 	backpack := backpack.Backpack{
-		Port:      8080,
-		Routes:    []backpack.Route{},
-		Handle405: handleWrongMethod,
+		Port:   8080,
+		Routes: make(map[string]backpack.Route),
 	}
 
 	backpack.Get("/", handleHome)
+	//TODO: figure out how to make middleware work
+	backpack.RegisterMiddleware("GET/", logMiddleware)
 
 	backpack.Serve()
 }
@@ -21,6 +23,6 @@ func handleHome(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Welcome home"))
 }
 
-func handleWrongMethod(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Method not supported"))
+func logMiddleware() {
+	fmt.Println("logging")
 }
