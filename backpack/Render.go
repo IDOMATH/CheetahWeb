30c0d0dfb63b590347a0d28719862c7bb94ece55
IDOMATH <btdothemath@gmail.com>
2023-12-26 15:11:54 -0600

    Adding template cache creation and rendering templates

--- backpack/Render.go
+++ backpack/Render.go
@@ -1,8 +1,12 @@
 package backpack
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
 	"html/template"
+	"log"
+	"net/http"
 	"path/filepath"
 )
 
@@ -13,8 +17,16 @@ type Renderer struct {
 	UseCache         bool
 }
 
+type TemplateData struct {
+	PageTitle string
+	BoolMap   map[string]bool
+	StringMap map[string]string
+	IntMap    map[string]string
+	ObjectMap map[string]interface{}
+}
+
 func NewRenderer(templateLocation, layoutLocation string, useCache bool) (*Renderer, error) {
-	cache := map[string]*template.Template{}
+	var cache map[string]*template.Template
 
 	renderer := &Renderer{
 		TemplateLocation: templateLocation,
@@ -22,33 +34,76 @@ func NewRenderer(templateLocation, layoutLocation string, useCache bool) (*Rende
 		TemplateCache:    cache,
 		UseCache:         useCache,
 	}
-
-	pages, err := filepath.Glob(fmt.Sprintf("%s/*.go.html", templateLocation))
+	cache, err := renderer.createTemplateCache()
 	if err != nil {
 		return renderer, err
+	}
+
+	return renderer, nil
+}
+
+func (ren *Renderer) Template(w http.ResponseWriter, r *http.Request, tmpl string, data TemplateData) error {
+	var tc map[string]*template.Template
+	var err error
+
+	if ren.UseCache {
+		tc = ren.TemplateCache
+	} else {
+		tc, err = ren.createTemplateCache()
+	}
+	if err != nil {
+		return err
+	}
+
+	t, ok := tc[tmpl]
+	if !ok {
+		return errors.New("can't get template from cache")
+	}
+
+	buf := new(bytes.Buffer)
+
+	err = t.Execute(buf, data)
+	if err != nil {
+		//TODO: probably shouldn't explode when a template fails to execute.
+		log.Fatal(err)
+	}
+
+	_, err = buf.WriteTo(w)
+	if err != nil {
+		fmt.Println("Error writing template to browser", err)
+		return err
+	}
+
+	return nil
+}
+
+func (r *Renderer) createTemplateCache() (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob(fmt.Sprintf("%s/*.go.html", r.TemplateLocation))
+	if err != nil {
+		return cache, err
 	}
 
 	for _, page := range pages {
 		name := filepath.Base(page)
 		template, err := template.New(name).ParseFiles(page)
 		if err != nil {
-			return renderer, err
+			return cache, err
 		}
 
-		matches, err := filepath.Glob(fmt.Sprintf("%s/*.go.html", layoutLocation))
+		matches, err := filepath.Glob(fmt.Sprintf("%s/*.go.html", r.LayoutLocation))
 		if err != nil {
-			return renderer, err
+			return cache, err
 		}
 
 		if len(matches) > 0 {
 			template, err = template.ParseGlob(fmt.Sprintf("%s/*.go.html"))
 			if err != nil {
-				return renderer, err
+				return cache, err
 			}
 		}
 		cache[name] = template
 	}
-	return renderer, nil
+	return cache, nil
 }
-
-// TODO: Add the actual render function
--- backpack/backpack.go
+++ backpack/backpack.go
@@ -8,6 +8,7 @@ import (
 type Backpack struct {
 	Port      int
 	Routes    map[string]Route
+	Renderer  Renderer
 	Handle405 http.HandlerFunc
 }
 
